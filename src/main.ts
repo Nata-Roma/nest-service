c3874bb32cf010d258a8076806c7f2ee1fd0b3d0
Natalia Romashko <sharkellgm@gmail>
2022-07-13 17:43:39 +0300

    add port number by default

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const PORT = configService.get<string>('PORT');
+  const PORT = configService.get<string>('PORT') || 4000;
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
 
   const DOC_API = await readFile(resolve(cwd(), 'doc', 'api.yaml'), 'utf-8');
